31e0602c3ebf7e74e41f8572311341f8612528ae
Ale <alessandro.luzzi77@gmail.com>
2016-07-23 14:23:18 +0200

    risolto problema font

--- Laboratorio/nuovo_lotto_cucina.py
+++ Laboratorio/nuovo_lotto_cucina.py
@@ -85,9 +85,6 @@ class NuovoLottoCucina(tk.Toplevel):
         '''
         BOTTONE ESCI E SALVA
         '''
-        self.stile = ttk.Style()
-        self.stile.configure('TButton', font='Helvetica')
-
         self.btn_invia = ttk.Button(self.frame_basso, text="Invio", command=self.invia)
         self.btn_esci = ttk.Button(self.frame_basso, text="Chiudi finestra", command=self.destroy)
         self.btn_esci_salva = ttk.Button(self.frame_basso, text="Esci e salva", command=self.esci_salva)
