e71ebb6184b2d1ec80d06225f357b45eeccfdbab
Ale <alessandro.luzzi77@gmail.com>
2016-11-17 18:07:30 +0100

    aggiunta combobox per gestione merceologia degli ingredienti base

--- Laboratorio/anagrafica_ingredienti.py
+++ Laboratorio/anagrafica_ingredienti.py
@@ -52,6 +52,11 @@ class Ingredienti(tk.Frame):
         self.lbl_frame_dettagli_selezionato = ttk.LabelFrame(self.frame_dx, text='Dettagli ingrediente selezionato')
         self.lbl_frame_attributi_ingrediente = ttk.LabelFrame(self.frame_dx, text='Attributi ingrediente selezionato')
         '''
+        Combobox per gestire merceologia prodotti
+        '''
+        self.box_merceologia = tk.StringVar()
+        self.box = ttk.Combobox(self.lbl_frame_attributi_ingrediente, textvariable=self.box_merceologia)
+        '''
         Labelframe scegli ingrediente
         '''
         self.lbl_frame_scegli = ttk.LabelFrame(self.frame_dx, text='Azioni')
@@ -66,6 +71,7 @@ class Ingredienti(tk.Frame):
         self.tree_ingredienti.grid(row=1, column=0, columnspan=3, sticky='we')
         self.lbl_frame_dettagli_selezionato.grid(row=1, column=0, sticky='n')
         self.lbl_frame_attributi_ingrediente.grid(row=2, column=0)
+        self.box.grid(columnspan=2)
 
         self.lbl_frame_scegli.grid(row=3, column=0)
         self.btn_modifica.grid()
