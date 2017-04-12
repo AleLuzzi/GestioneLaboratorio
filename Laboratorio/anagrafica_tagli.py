3519a7bde24c53eb488d063e80d05c1b9c48ed98
Ale <alessandro.luzzi77@gmail.com>
2017-04-12 14:40:43 +0200

    miglioramenti grafici

--- Laboratorio/anagrafica_tagli.py
+++ Laboratorio/anagrafica_tagli.py
@@ -8,22 +8,18 @@ class Tagli(tk.Frame):
         tk.Frame.__init__(self, parent)
 
         self.item = ''
-        # TODO aggiungere flag per produzione
-        '''
-        Connessione al Database
-        '''
+
+        # Connessione al Database
         self.conn = sqlite3.connect('data.db',
                                     detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
         self.c = self.conn.cursor()
-        '''
-        Definizione Frame
-        '''
+
+        # Definizione Frame
         self.frame_sx = ttk.Frame(self)
         self.frame_centrale = ttk.Frame(self)
         self.frame_dx = ttk.Frame(self)
-        '''
-        Treeview per tab Tagli
-        '''
+
+        # TREEVIEEW per tab Tagli
         self.tree_tagli = ttk.Treeview(self.frame_sx, height=23)
         self.tree_tagli['columns'] = ('Id', 'Tagli')
         self.tree_tagli['show'] = 'headings'
@@ -34,55 +30,61 @@ class Tagli(tk.Frame):
         self.tree_tagli.column("Tagli", width=150)
 
         self.tree_tagli.bind("<Double-1>", self.ondoubleclick)
-        '''
-        Lista campi del record
-        '''
+
+        # Lista campi del record
         self.campi = ['taglio']
         self.label = {}
         self.entry = {}
-        '''
-        Labelframe dettagli taglio selezionato
-        '''
+
+        # LABELFRAME dettagli taglio selezionato
         self.lbl_frame_taglio_selezionato = ttk.LabelFrame(self.frame_centrale, text='Dettagli taglio selezionato')
-        '''
-        Labelframe Azioni e filtra
-        '''
-        self.lbl_frame_scegli = ttk.LabelFrame(self.frame_centrale, text='Azioni')
-        self.lbl_frame_filtra = ttk.Labelframe(self.frame_centrale, text='Filtra')
-        '''
-        Bottoni
-        '''
-        self.btn_modifica = ttk.Button(self.lbl_frame_scegli, text='Modifica', command=self.modifica)
-        self.btn_inserisci = ttk.Button(self.lbl_frame_scegli, text='Inserisci', command=self.inserisci)
-        self.btn_applica_filtro = ttk.Button(self.lbl_frame_filtra, text='Applica', command=self.filtra)
-        '''
-        Combobox per gestire rimpimento tramite classe prodotti
-        '''
+
+        # LABELFRAME Azioni e filtra
+        self.lbl_frame_scegli = ttk.LabelFrame(self.frame_dx,
+                                               text='')
+        self.lbl_frame_filtra = ttk.Labelframe(self.frame_centrale,
+                                               text='Filtra')
+
+        # BOTTONI
+        self.btn_modifica = tk.Button(self.lbl_frame_scegli,
+                                      text='Salva Modifiche',
+                                      font=('Helvetica', 10),
+                                      command=self.modifica)
+        self.btn_inserisci = tk.Button(self.lbl_frame_scegli,
+                                       text='Inserisci Dati',
+                                       font=('Helvetica', 10),
+                                       command=self.inserisci)
+        self.btn_applica_filtro = tk.Button(self.lbl_frame_filtra,
+                                            text='Applica',
+                                            font=('Helvetica', 10),
+                                            command=self.filtra)
+
+        # COMBOBOX per gestire rimpimento tramite classe prodotti
         self.box_value = tk.StringVar()
         self.box = ttk.Combobox(self.lbl_frame_filtra, textvariable=self.box_value)
-        '''
-        Labelframe Attributi Taglio
-        '''
+
+        # LABELFRAME Attributi Taglio
         self.lbl_frame_attributi_taglio = ttk.LabelFrame(self.frame_centrale,
                                                          text='Attributi taglio selezionato')
         self.flag1 = tk.Checkbutton(self.lbl_frame_attributi_taglio, text='Modulo Inventario')
-        '''
-        LAYOUT
-        '''
-        self.frame_sx.grid(row='1', column='0', sticky='n')
-        self.frame_centrale.grid(row='1', column='1', sticky='n')
-        self.frame_dx.grid(row='1', column='2', sticky='n')
 
-        self.tree_tagli.grid(row='1', column='0', columnspan='3', sticky='we')
-        self.lbl_frame_taglio_selezionato.grid(row='1', column='0', sticky='n')
+        # LAYOUT
+        self.frame_sx.grid(row=1, column=0, sticky='n')
+        self.frame_centrale.grid(row=1, column=1, sticky='n')
+        self.frame_dx.grid(row=1, column=2, sticky='n')
+
+        self.tree_tagli.grid(row=1, column=0, columnspan=3, sticky='we')
+        self.lbl_frame_taglio_selezionato.grid(row=1, column=0, sticky='n')
         self.lbl_frame_attributi_taglio.grid(row=2, column=0)
         self.flag1.grid()
-        self.lbl_frame_scegli.grid(row='4', column='0')
-        self.lbl_frame_filtra.grid(row=3, column=0)
-        self.btn_modifica.grid()
-        self.btn_inserisci.grid()
+        self.lbl_frame_scegli.grid(row=4, column=0)
+        self.lbl_frame_filtra.grid(row=3, column=0, sticky='we')
+
+        self.btn_modifica.grid(sticky='we')
+        self.btn_inserisci.grid(sticky='we')
+
         self.box.grid()
-        self.btn_applica_filtro.grid()
+        self.btn_applica_filtro.grid(sticky='we')
 
         self.aggiorna()
         self.crea_label_entry()
