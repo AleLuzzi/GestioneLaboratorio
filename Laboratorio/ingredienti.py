b62470c747a732939aa3c27a8803106959bd77d1
Ale <alessandro.luzzi77@gmail.com>
2017-05-02 19:55:59 +0200

    rivisto il salvataggio dei dati aggiunto bottone per la rimozione delle righe da salvare miglioramenti grafici

--- Laboratorio/ingredienti.py
+++ Laboratorio/ingredienti.py
@@ -15,40 +15,56 @@ class Ingredienti(tk.Toplevel):
         self.peso = tk.StringVar()
         self.peso.set('')
         self.img_btn1 = tk.PhotoImage(file=".//immagini//logo_piccolo.gif")
-        '''
-        Definizione Frame
-        '''
+
+        # FRAME definizione
         self.frame_sx = tk.Frame(self, bd=3, relief='groove')
         self.frame_dx = tk.Frame(self, bd=3, relief='groove')
         self.frame_basso = tk.Frame(self, background='white')
-        '''
-        Treeview per riepilogo ingredienti inseriti
-        '''
+
+        # LABEL che mostra il numero della settimana
+        self.data = datetime.date.today()
+        self.n_sett = str(1 + int(self.data.strftime('%W')))
+        self.lbl_settimana = ttk.Label(self.frame_sx, text='SETTIMANA NUMERO ' + self.n_sett,
+                                       foreground='blue', font=('Verdana', 20))
+        self.label_data_utilizzo = ttk.Label(self.frame_sx,
+                                             text="Data Utilizzo Merce", foreground='blue', font=('Verdana', 20))
+        self.label_data = ttk.Label(self.frame_sx, anchor='center',
+                                    text=self.data.strftime('%d/%m/%y'), font=('Verdana', 20))
+
+        # TREEVIEW per riepilogo ingredienti inseriti
         self.tree = ttk.Treeview(self.frame_sx)
-        self.tree['columns'] = ('articolo', 'quantita')
+        self.tree['columns'] = ('n_settimana', 'articolo', 'quantita', 'data')
         self.tree['show'] = 'headings'
+        self.tree['displaycolumns'] = ('articolo', 'quantita')
         self.tree.column("articolo", width=200)
         self.tree.column("quantita", width=100)
         self.tree.heading("articolo", text="articolo")
         self.tree.heading("quantita", text="quantita")
-        '''
-        Label che mostra il numero della settimana
-        '''
-        self.data = datetime.date.today()
-        self.n_sett = str(1 + int(self.data.strftime('%W')))
-        self.lbl_settimana = ttk.Label(self.frame_sx, text='SETTIMANA NUMERO ' + self.n_sett,
-                                       foreground='blue', font=('Helvetica', 20))
-        self.label_data_utilizzo = ttk.Label(self.frame_sx,
-                                             text="Data Utilizzo Merce", foreground='blue', font=('Helvetica', 20))
-        self.label_data = ttk.Label(self.frame_sx, anchor='center',
-                                    text=self.data.strftime('%d/%m/%y'), font=('Helvetica', 20))
-        '''
-        Notebook e posizione
-        '''
+
+        # BOTTONE elimina riga
+        self.btn_elimina_riga = tk.Button(self.frame_sx,
+                                          text='Elimina riga',
+                                          font=('Verdana', 15),
+                                          command=self.rimuovi_riga_selezionata)
+
+        # LABEL peso da inserire
+        self.lbl_peso = ttk.Label(self.frame_sx, text='Quantità merce utilizzata',
+                                  foreground='blue',
+                                  font=('Verdana', 15))
+
+        # ENTRY peso da inserire
+        self.entry_peso = ttk.Entry(self.frame_sx,
+                                    textvariable=self.peso,
+                                    font=('Verdana', 10))
+
+        # LABELFRAME per inserimento EAN
+        self.lblfr_ins_ean = tk.LabelFrame(self.frame_sx, text='Inserimento ean', font=('Verdana', 15))
+
+        # NOTEBOOK e posizione
         self.notebook = ttk.Notebook(self.frame_dx)
-        '''
-        Definizione dizionari per ingredienti
-        '''
+        self.notebook.bind_all("<<NotebookTabChanged>>", self.rimetti_focus)
+
+        # Definizione dizionari per ingredienti
         self.lista_surgelati = {'spinaci': '1950', 'cicoria': '1973', 'seppia': '2033', 'baccala': '2038',
                                 'pangasio': '2025', 'frittura pesce': '1984', 'spiedini pesce': '2024',
                                 'carciofi': '2012', 'fil.gallinella': '2052', 'gamberi 50-60': '1992',
@@ -67,138 +83,95 @@ class Ingredienti(tk.Toplevel):
                             'pancia di vitella': '030140', 'coniglio': '030185', 'roastbeef vitella': '030162',
                             'filetto suino': '030110', 'coscio agnello': '030103', 'fegato suino': '030169',
                             'paliata vit.': '030243', 'faraona': '030158', 'anatra': '030147', 'arista suino': '030166'}
-        '''
-        TAB 1 per SURGELATI
-        '''
+
+        # TAB 1 per SURGELATI
         self.tab1 = ttk.Frame(self.notebook)
         self.notebook.add(self.tab1, text='SURGELATI', compound='left', image=self.img_btn1)
-        '''
-        ciclo per creazione bottoni surgelati
-        '''
-        self.crea_bottoni_surgelati()
 
-        ttk.Separator(self.tab1, orient='vertical').grid(row='1', column='2', rowspan=8, sticky='ns', padx=5)
-
-        self.lbl_peso_s = ttk.Label(self.tab1, text='PESO', width=20, anchor='center')
-        self.lbl_peso_s.grid(row='1', column='3', columnspan='2')
-
-        self.entry_peso_s = ttk.Entry(self.tab1, textvariable=self.peso)
-        self.entry_peso_s.focus()
-        self.entry_peso_s.grid(row='2', column='4', columnspan='2', sticky='we')
-        '''
-        TAB 2 per FRESCHI
-        '''
+        # TAB 2 per FRESCHI
         self.tab2 = ttk.Frame(self.notebook)
         self.notebook.add(self.tab2, text='FRESCHI', compound='left', image=self.img_btn1)
-        '''
-        ciclo per creazione bottoni freschi
-        '''
-        self.crea_bottoni_freschi()
 
-        ttk.Separator(self.tab2, orient='vertical').grid(row='1', column='1', rowspan=5, sticky='ns', padx=5)
-
-        self.lbl_peso_f = ttk.Label(self.tab2, text='PESO', width=20, anchor='center')
-        self.lbl_peso_f.grid(row='1', column='2', columnspan='2')
-
-        self.entry_peso_f = ttk.Entry(self.tab2, textvariable=self.peso)
-        self.entry_peso_f.focus()
-        self.entry_peso_f.grid(row='2', column='2', columnspan='2', sticky='we')
-        '''
-        TAB 3 per codice EAN
-        '''
-        self.tab3 = ttk.Frame(self.notebook)
-        self.notebook.add(self.tab3, text='CODICE EAN', compound='left', image=self.img_btn1)
-
-        self.lbl_ean = ttk.Label(self.tab3, text='EAN', width=20, anchor='center')
-        self.lbl_ean.grid(row='1', column='0')
-
-        self.ean = tk.StringVar()
-        self.ean.set('')
-        self.entry_ean = ttk.Entry(self.tab3, textvariable=self.ean)
-        self.entry_ean.grid(row='2', column='0')
-
-        self.lbl_pezzi = ttk.Label(self.tab3, text='PEZZI', width=20, anchor='center')
-        self.lbl_pezzi.grid(row='3', column='0')
-
-        self.pezzi = tk.StringVar()
-        self.pezzi.set('')
-        self.entry_pezzi = ttk.Entry(self.tab3, textvariable=self.pezzi)
-        self.entry_pezzi.grid(row='4', column='0')
-
-        self.img_btn_focus_ean = tk.PhotoImage(file=".//immagini//modifica.gif")
-        self.btn_focus_ean = ttk.Button(self.tab3, image=self.img_btn_focus_ean, command=self.entry_ean.focus)
-        self.btn_focus_ean.grid(row=2, column=1, padx=10)
-
-        self.img_btn_focus_pezzi = tk.PhotoImage(file=".//immagini//modifica.gif")
-        self.btn_focus_pezzi = ttk.Button(self.tab3, image=self.img_btn_focus_pezzi, command=self.entry_pezzi.focus)
-        self.btn_focus_pezzi.grid(row=4, column=1, padx=10)
-        '''
-        TAB 4 per pasta fresca
-        '''
+        # TAB 3 per pasta fresca
         self.tab4 = ttk.Frame(self.notebook)
         self.notebook.add(self.tab4, text='PASTA FRESCA', compound='left', image=self.img_btn1)
-        '''
-        ciclo per creazione bottoni pasta fresca
-        '''
-        self.crea_bottoni_pasta_fresca()
 
-        self.lbl_peso_pf = ttk.Label(self.tab4, text='PESO', width=20, anchor='center')
-        self.lbl_peso_pf.grid(row='1', column='2', columnspan='2')
-
-        self.entry_peso_pf = ttk.Entry(self.tab4, textvariable=self.peso)
-        self.entry_peso_pf.focus()
-        self.entry_peso_pf.grid(row='2', column='2', columnspan='2', sticky='we')
-        '''
-        TAB 5 per CARNE
-        '''
+        # TAB 4 per CARNE
         self.tab5 = ttk.Frame(self.notebook)
         self.notebook.add(self.tab5, text='CARNE', compound='left', image=self.img_btn1)
-        '''
-        ciclo per creazione bottoni carne
-        '''
-        self.crea_bottoni_carne()
 
-        self.lbl_peso_c = ttk.Label(self.tab5, text='PESO', width=20, anchor='center')
-        self.lbl_peso_c.grid(row='1', column='3', columnspan='2')
+        # LABEL ed ENTRY per inserimento EAN
+        self.lbl_ean = ttk.Label(self.lblfr_ins_ean, text='EAN', width=20, anchor='center')
+        self.ean = tk.StringVar()
+        self.ean.set('')
+        self.entry_ean = ttk.Entry(self.lblfr_ins_ean, textvariable=self.ean)
 
-        self.entry_peso_c = ttk.Entry(self.tab5, textvariable=self.peso)
-        self.entry_peso_c.focus()
-        self.entry_peso_c.grid(row='2', column='3', columnspan='2', sticky='we')
-        '''
-        BOTTONI per inserimento salvataggio e chiusura finestra
-        '''
+        self.lbl_pezzi = ttk.Label(self.lblfr_ins_ean, text='PEZZI', width=20, anchor='center')
+        self.pezzi = tk.StringVar()
+        self.pezzi.set('')
+        self.entry_pezzi = ttk.Entry(self.lblfr_ins_ean, textvariable=self.pezzi)
+
+        self.img_btn_focus_ean = tk.PhotoImage(file=".//immagini//modifica.gif")
+        self.btn_focus_ean = ttk.Button(self.lblfr_ins_ean, image=self.img_btn_focus_ean, command=self.entry_ean.focus)
+
+        self.img_btn_focus_pezzi = tk.PhotoImage(file=".//immagini//modifica.gif")
+        self.btn_focus_pezzi = ttk.Button(self.lblfr_ins_ean, image=self.img_btn_focus_pezzi,
+                                          command=self.entry_pezzi.focus)
+
+        # BOTTONI per inserimento salvataggio e chiusura finestra
         self.btn_invio = tk.Button(self.frame_basso,
                                    text="Conferma",
-                                   font=('Helvetica', 15),
-                                   width=25,
+                                   font=('Verdana', 15),
+                                   width=20,
                                    command=self.invio)
         self.btn_salva = tk.Button(self.frame_basso,
                                    text='salva',
-                                   font=('Helvetica', 15),
-                                   width=25,
+                                   font=('Verdana', 15),
+                                   width=20,
                                    command=self.salva)
         self.btn_chiudi = tk.Button(self.frame_basso,
                                     text="Chiudi finestra",
-                                    font=('Helvetica', 15),
-                                    width=25,
+                                    font=('Verdana', 15),
+                                    width=20,
                                     command=self.destroy)
-        self.btn_invio.grid(row='3', column='0', padx=20, pady=17)
-        self.btn_salva.grid(row='3', column='1', padx=20, pady=17)
-        self.btn_chiudi.grid(row='3', column='2', padx=20, pady=17)
-        '''
-        LAYOUT
-        '''
+
+        # LAYOUT
         self.frame_sx.grid(row=0, column=0, sticky='ns')
         self.frame_dx.grid(row=0, column=1)
         self.frame_basso.grid(row=1, column=0, columnspan=2, sticky='we')
 
-        self.tree.grid(row=3, column=0, columnspan=2)
+        self.lbl_settimana.grid(row=0, column=0, columnspan=2)
 
+        self.label_data_utilizzo.grid(row=1, column=0)
         self.label_data.grid(row=1, column=1)
 
+        self.tree.grid(row=3, column=0, pady=5)
+        self.btn_elimina_riga.grid(row=3, column=1, sticky='n')
+
+        self.lbl_peso.grid(row=4, column=0)
+        self.entry_peso.grid(row=4, column=1)
+
+        self.lblfr_ins_ean.grid(row=5, column=0, columnspan=2)
+        self.lbl_ean.grid(row=1, column=0)
+        self.entry_ean.grid(row=1, column=1)
+        self.lbl_pezzi.grid(row=2, column=0)
+        self.entry_pezzi.grid(row=2, column=1)
+        self.btn_focus_ean.grid(row=1, column=2)
+        self.btn_focus_pezzi.grid(row=2, column=2)
+
         self.notebook.grid(row=0, column=0)
-        self.lbl_settimana.grid(row=0, column=0, columnspan=2)
-        self.label_data_utilizzo.grid(row=1, column=0)
+
+        self.btn_invio.grid(row=3, column=0, padx=20, pady=17)
+        self.btn_salva.grid(row=3, column=1, padx=20, pady=17)
+        self.btn_chiudi.grid(row=3, column=2, padx=20, pady=17)
+
+        self.entry_peso.focus()
+
+        # cicli per creazione bottoni
+        self.crea_bottoni_surgelati()
+        self.crea_bottoni_freschi()
+        self.crea_bottoni_pasta_fresca()
+        self.crea_bottoni_carne()
 
     def crea_bottoni_surgelati(self):
         r, c = 1, 0
@@ -206,7 +179,7 @@ class Ingredienti(tk.Toplevel):
             if r % 10 == 0:
                 c += 1
                 r = 1
-            tk.Radiobutton(self.tab1, text=k.upper(), indicatoron=0, variable=self.value, font='Helvetica', width=20,
+            tk.Radiobutton(self.tab1, text=k.upper(), indicatoron=0, variable=self.value, font='Verdana', width=22,
                            value=k + ' cod.' + v).grid(row=r, column=c)
             r += 1
 
@@ -216,7 +189,7 @@ class Ingredienti(tk.Toplevel):
             if r % 10 == 0:
                 c += 1
                 r = 1
-            tk.Radiobutton(self.tab2, text=k.upper(), indicatoron=0, variable=self.value, font='Helvetica', width=20,
+            tk.Radiobutton(self.tab2, text=k.upper(), indicatoron=0, variable=self.value, font='Verdana', width=20,
                            value=k + ' cod.' + v).grid(row=r, column=c)
             r += 1
 
@@ -226,7 +199,7 @@ class Ingredienti(tk.Toplevel):
             if r % 10 == 0:
                 c += 1
                 r = 1
-            tk.Radiobutton(self.tab4, text=k.upper(), indicatoron=0, variable=self.value, font='Helvetica', width=20,
+            tk.Radiobutton(self.tab4, text=k.upper(), indicatoron=0, variable=self.value, font='Verdana', width=20,
                            value=k + ' cod.' + v).grid(row=r, column=c)
             r += 1
 
@@ -236,19 +209,24 @@ class Ingredienti(tk.Toplevel):
             if r % 15 == 0:
                 c += 1
                 r = 1
-            tk.Radiobutton(self.tab5, text=k.upper(), indicatoron=0, variable=self.value, font='Helvetica', width=20,
+            tk.Radiobutton(self.tab5, text=k.upper(), indicatoron=0, variable=self.value, font='Verdana', width=20,
                            value=k + ' cod.' + v).grid(row=r, column=c)
             r += 1
 
+    def rimetti_focus(self, event):
+        self.entry_peso.focus()
+
+    def rimuovi_riga_selezionata(self):
+            curitem = self.tree.selection()[0]
+            self.tree.delete(curitem)
+
     def invio(self):
         if self.value.get() != '':
-            self.tree.insert("", 0, values=(self.value.get(), self.peso.get()))
-            self.lista_da_salvare.append((self.n_sett, (self.value.get()), (self.peso.get()), self.data))
-            self.entry_peso_s.delete(0, tk.END)
+            self.tree.insert("", 0, values=(self.n_sett, self.value.get(), self.peso.get(), self.data))
+            self.entry_peso.delete(0, tk.END)
             self.value.set('')
-        elif self.ean.get() != '':
-            self.tree.insert("", 0, values=(self.ean.get(), self.pezzi.get()))
-            self.lista_da_salvare.append((self.n_sett, (self.ean.get()), (self.pezzi.get()), self.data))
+        elif self.ean.get() != '' and self.pezzi.get() != '':
+            self.tree.insert("", 0, values=(self.n_sett, self.ean.get(), self.pezzi.get(), self.data))
             self.entry_pezzi.delete(0, tk.END)
             self.ean.set('')
 
@@ -258,6 +236,9 @@ class Ingredienti(tk.Toplevel):
                                             user='root',
                                             password='')
         c = conn.cursor()
+        for child in self.tree.get_children():
+            self.lista_da_salvare.append(self.tree.item(child)['values'])
+        print(self.lista_da_salvare)
         c.executemany('INSERT INTO ingredienti VALUES (%s,%s,%s,%s)', self.lista_da_salvare)
         conn.commit()
         conn.close()
