cad44a8aa86cc66ef939d40d1d6c06fd9103c126
AleLuzzi <alessandro.luzzi77@gmail.com>
2018-10-16 15:27:41 +0200

    aggiunto Treeview lotti in uscita

--- Laboratorio/report_cucina_note.py
+++ Laboratorio/report_cucina_note.py
@@ -6,70 +6,76 @@ from reportlab.lib.units import inch
 from reportlab.lib.pagesizes import A4
 from reportlab.platypus import SimpleDocTemplate, Spacer, Table, TableStyle
 import mysql.connector
-import os
+# import os
 from tkinter import messagebox
 import win32api
+
 
 class ReportCucina(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
-        '''
-        Connessione al database
-        '''
+
+        # Connessione al database
         self.conn = mysql.connector.connect(host='192.168.0.100',
                                             database='data',
                                             user='root',
                                             password='')
         self.c = self.conn.cursor()
-        '''
-        Treeview per riepilogo immissioni
-        '''
-        self.tree = ttk.Treeview(self, height=20)
-        self.tree['columns'] = ('Prodotto', 'Quantita')
-        self.tree['show'] = 'headings'
-        self.tree.heading('Prodotto', text="Prodotto")
-        self.tree.heading('Quantita', text="Quantita")
 
-        self.tree.column("Prodotto", width=200)
-        self.tree.column("Quantita", width=100)
+        # Treeview per merce ingresso
+        self.tree_ingresso = ttk.Treeview(self, height=20)
+        self.tree_ingresso['columns'] = ('Prodotto', 'Quantita')
+        self.tree_ingresso['show'] = 'headings'
+        self.tree_ingresso.heading('Prodotto', text="Prodotto")
+        self.tree_ingresso.heading('Quantita', text="Quantita")
 
-        self.tree.grid(row='0', column='0')
-        '''
-        Labelframe per contenere i bottoni
-        '''
+        self.tree_ingresso.column("Prodotto", width=200)
+        self.tree_ingresso.column("Quantita", width=100)
+
+        # Treeview per merce in uscita
+        self.tree_uscita = ttk.Treeview(self, height=20)
+        self.tree_uscita['columns'] = ('Prodotto', 'Quantita')
+        self.tree_uscita['show'] = 'headings'
+        self.tree_uscita.heading('Prodotto', text="Prodotto")
+        self.tree_uscita.heading('Quantita', text="Quantita")
+
+        self.tree_uscita.column("Prodotto", width=200)
+        self.tree_ingresso.column("Quantita", width=100)
+
+        # Labelframe per contenere i bottoni
         self.lblFrame_azioni = ttk.Labelframe(self, text='Azioni')
-        self.lblFrame_azioni.grid(row='0', column='1', sticky='n')
-        '''
-        Label che indica la settimana
-        '''
+
+        # Label che indica la settimana
         n_sett = str(1 + int(strftime('%W')))
         self.lbl_settimana = ttk.Label(self.lblFrame_azioni, text='SETTIMANA NUMERO ' + n_sett, font='Helvetica')
-        self.lbl_settimana.grid()
-        '''
-        Bottone per popolare il treeview
-        '''
-        self.btn = ttk.Button(self.lblFrame_azioni, text='Inserisci dati', command=self.inserisci)
-        self.btn.grid()
-        '''
-        Entry per scegliere la settimana
-        '''
-        self.lbl_scelta_sett = ttk.Label(self.lblFrame_azioni, text='Settimana da visualizzare ')
-        self.lbl_scelta_sett.grid()
 
+        # Bottone per popolare il treeview
+        self.btn = ttk.Button(self.lblFrame_azioni, text='Inserisci dati', command=self.inserisci)
+
+        # Entry per scegliere la settimana
+        self.lbl_scelta_sett = ttk.Label(self.lblFrame_azioni, text='Settimana da visualizzare ')
         self.settimana = tk.StringVar()
         self.settimana.set('')
         self.entry_settimana_s = ttk.Entry(self.lblFrame_azioni, textvariable=self.settimana)
         self.entry_settimana_s.focus()
-        self.entry_settimana_s.grid()
-        '''
-        Bottone per generare la stampa del pdf
-        '''
+
+        # Bottone per generare la stampa del pdf
         self.stampa = ttk.Button(self.lblFrame_azioni, text="Crea PDF", command=self.crea_pdf)
+
+        # LAYOUT
+
+        self.tree_ingresso.grid(row=0, column=0)
+        self.tree_uscita.grid(row=0, column=1)
+        self.lblFrame_azioni.grid(row=0, column=2, sticky='n')
+        self.lbl_settimana.grid()
+        self.lbl_scelta_sett.grid()
+        self.btn.grid()
+        self.entry_settimana_s.grid()
         self.stampa.grid()
 
     def inserisci(self):
 
-        self.tree.delete(*self.tree.get_children())
+        self.tree_ingresso.delete(*self.tree_ingresso.get_children())
 
         self.c.execute('SELECT prodotto,SUM(quantita) '
                        'FROM ingredienti '
@@ -77,7 +83,7 @@ class ReportCucina(tk.Frame):
                        'GROUP BY prodotto',
                        (self.settimana.get(),))
         for row in self.c:
-            self.tree.insert("", 'end', values=(row[0], row[1]))
+            self.tree_ingresso.insert("", 'end', values=(row[0], row[1]))
 
     def crea_pdf(self):
         data = [('settimana', 'codice', 'prodotto')]
@@ -117,5 +123,5 @@ if __name__ == '__main__':
     notebook = ttk.Notebook(root)
     notebook.grid(row='1', column='0')
     new = ReportCucina(notebook)
-    notebook.add(new, text='Dipendenti')
+    notebook.add(new, text='Report cucina')
     root.mainloop()
