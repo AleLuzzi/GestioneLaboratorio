36a912ebab1314df71eb4887f5375e4f7540016b
AleLuzzi <alessandro.luzzi77@gmail.com>
2019-02-06 15:51:34 +0100

    miglioramenti

--- Laboratorio/inventario.py
+++ Laboratorio/inventario.py
@@ -24,7 +24,7 @@ class Inventario(tk.Toplevel):
 
         # Definizione Frame
         self.frame_sx = tk.Frame(self)
-        self.frame_alto = tk.Frame(self, bd='3', relief='groove')
+        # self.frame_alto = tk.Frame(self, bd='3', relief='groove')
         self.frame_dx = tk.Frame(self)
 
         self.notebook = ttk.Notebook(self.frame_dx)
@@ -40,23 +40,23 @@ class Inventario(tk.Toplevel):
         self.tree_riepilogo.column("Peso", width=80)
 
         # LABEL che mostra il numero della settimana
-        self.lbl_settimana = tk.Label(self.frame_alto, text='SETTIMANA NUMERO ',
+        self.lbl_settimana = tk.Label(self.frame_dx, text='SETTIMANA NUMERO ',
                                       foreground='blue', font=('Verdana', 20), relief='ridge', padx=20)
-        self.lbl_nr_settimana = tk.Label(self.frame_alto, text=str(1 + int(self.data.strftime('%W'))),
+        self.lbl_nr_settimana = tk.Label(self.frame_dx, text=str(1 + int(self.data.strftime('%W'))),
                                          font=('Verdana', 20), bg='white', relief='sunken', padx=20)
 
         # LAYOUT
-        self.frame_sx.grid(row=0, column=0, rowspan=2)
-        self.frame_alto.grid(row=0, column=1)
-        self.frame_dx.grid(row=1, column=1, sticky='n')
+        self.frame_sx.grid(row=0, column=0)
+        # self.frame_alto.grid(row=0, column=1)
+        self.frame_dx.grid(row=0, column=1, sticky='n')
 
         self.tree_riepilogo.grid()
-        self.lbl_settimana.grid(row=0, column=1)
-        self.lbl_nr_settimana.grid(row=0, column=2)
-        self.notebook.grid()
+        self.lbl_settimana.grid(row=0, column=1, sticky='we')
+        self.lbl_nr_settimana.grid(row=0, column=2, sticky='we')
+        self.notebook.grid(row=1, column=1, columnspan=2)
 
         # TAB 1 AGNELLO
-        self.tab1 = ttk.Frame(self.notebook)
+        self.tab1 = tk.Frame(self.notebook)
         self.notebook.add(self.tab1, text='AGNELLO', compound='left', image=self.img_btn1)
 
         lst_agnello = []
@@ -75,7 +75,7 @@ class Inventario(tk.Toplevel):
             r += 1
 
         # TAB 2 BOVINO
-        self.tab2 = ttk.Frame(self.notebook)
+        self.tab2 = tk.Frame(self.notebook)
         self.notebook.add(self.tab2, text='BOVINO', compound='left', image=self.img_btn1)
 
         lst_bovino = []
@@ -94,7 +94,7 @@ class Inventario(tk.Toplevel):
             r += 1
 
         # TAB 3 SUINO
-        self.tab3 = ttk.Frame(self.notebook)
+        self.tab3 = tk.Frame(self.notebook)
         self.notebook.add(self.tab3, text='SUINO', compound='left', image=self.img_btn1)
 
         lst_suino = []
@@ -113,7 +113,7 @@ class Inventario(tk.Toplevel):
             r += 1
 
         # TAB 4 VITELLO
-        self.tab4 = ttk.Frame(self.notebook)
+        self.tab4 = tk.Frame(self.notebook)
         self.notebook.add(self.tab4, text='VITELLO', compound='left', image=self.img_btn1)
 
         lst_vitello = []
