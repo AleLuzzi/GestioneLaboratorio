7bdaac9cad60eae7381839d5b15037ee52a31c73
Ale <alessandro.luzzi77@gmaail.com>
2020-10-10 12:22:26 +0200

    aggiunto codice btn reset

--- Laboratorio/dosi.py
+++ Laboratorio/dosi.py
@@ -23,12 +23,18 @@ class Dosi(tk.Toplevel):
             aromyl= self.peso.get()*8
             self.lbl_aromyl_gr['text'] = str(aromyl) + ' gr'
 
+        def _reset():
+            self.ent_peso.delete(0, 'end')
+
+        def _chiudi():
+            self.destroy()
+
+        # DEFINIZIONE WIDGET
         self.lbl_peso = tk.Label(self, text='INSERISCI PESO',
                                        foreground='blue',
                                        font=('Verdana', 15))
         self.ent_peso = tk.Entry(self, textvariable=self.peso, width=25)
         self.ent_peso.focus()
-        self.calcola = ttk.Button(self, text='CALCOLA', command=_calcola)
 
         self.lbl_sale = tk.Label(self, text='SALE')
         self.lbl_sale_gr = tk.Label(self, text='')
@@ -43,11 +49,14 @@ class Dosi(tk.Toplevel):
         self.lbl_aromyl_gr = tk.Label(self, text='')
         
         self.lbl_acqua = tk.Label(self, text='ACQUA')
-
+        
+        self.calcola = ttk.Button(self, text='CALCOLA', command=_calcola)
+        self.reset = ttk.Button(self, text='Reset', command=_reset)
+        self.chiudi = ttk.Button(self, text='Chiudi', command=_chiudi)
+        
+        # LAYOUT
         self.lbl_peso.grid(row=0, column=0)
         self.ent_peso.grid(row=0, column=1)
-        
-        self.calcola.grid(row=1, column=0, columnspan=2, sticky='we')
 
         self.lbl_sale.grid(row=2, column=0)
         self.lbl_sale_gr.grid(row=2, column=1)
@@ -62,6 +71,10 @@ class Dosi(tk.Toplevel):
         self.lbl_aromyl_gr.grid(row=5, column=1)
         
         self.lbl_acqua.grid(row=6, column=0)
+
+        self.calcola.grid(row=7, column=0, columnspan=2, sticky='we')
+        self.reset.grid(row=8, column=0, sticky='we')
+        self.chiudi.grid(row=8, column=1, sticky='we')
 
 
 if __name__ == "__main__":
