a11a83c55b09ade694f8ee5055b0467540fa988f
Ale <Alessandro>
2020-10-21 17:56:43 +0200

    aggiunto bind al tasto invio

--- Laboratorio/dosi.py
+++ Laboratorio/dosi.py
@@ -11,21 +11,21 @@ class Dosi(tk.Toplevel):
         self.peso = tk.IntVar()
         self.peso.set('')
 
-        def _calcola():
+        def _calcola(event=None):
             if self.peso.get() == 0 or self.peso.get() == 1:
                 messagebox.showinfo("-- ERRORE --", "Valore inserito non valido", icon="warning")
                 self.ent_peso.focus()
             if self.peso.get() > 1:
-                sale = self.peso.get()*26
+                sale = self.peso.get() * 26
                 self.lbl_sale_gr['text'] = str(sale) + ' gr'
 
-                pepe = self.peso.get()*5
+                pepe = self.peso.get() * 5
                 self.lbl_pepe_gr['text'] = str(pepe) + ' gr'
 
-                aglio = round(((self.peso.get()/100)*20), 2)
+                aglio = round(((self.peso.get() / 100) * 20), 2)
                 self.lbl_aglio_gr['text'] = str(aglio) + ' gr'
 
-                aromyl= self.peso.get()*8
+                aromyl = self.peso.get() * 8
                 self.lbl_aromyl_gr['text'] = str(aromyl) + ' gr'
 
         def _reset():
@@ -35,18 +35,20 @@ class Dosi(tk.Toplevel):
             self.lbl_aglio_gr['text'] = '0 gr'
             self.lbl_aromyl_gr['text'] = '0 gr'
             self.ent_peso.focus()
-            
+
         def _chiudi():
             self.destroy()
 
         # DEFINIZIONE WIDGET
+        self.bind('<Return>', _calcola)
+
         self.lbl_peso = tk.Label(self, text='INSERISCI PESO',
-                                       foreground='blue',
-                                       font=('Verdana', 15),
-                                       relief='ridge',
-                                       bg='white')
+                                 foreground='blue',
+                                 font=('Verdana', 15),
+                                 relief='ridge',
+                                 bg='white')
         self.ent_peso = tk.Entry(self, textvariable=self.peso,
-                                       width=5, font=('Verdana', 30))
+                                 width=5, font=('Verdana', 30))
         self.ent_peso.focus()
 
         self.lbl_sale = tk.Label(self, text='SALE', font=('Verdana', 15))
@@ -57,19 +59,19 @@ class Dosi(tk.Toplevel):
 
         self.lbl_aglio = tk.Label(self, text='AGLIO', font=('Verdana', 15))
         self.lbl_aglio_gr = tk.Label(self, text='0 gr', font=('Verdana', 15))
-        
+
         self.lbl_aromyl = tk.Label(self, text='AROMYL', font=('Verdana', 15))
         self.lbl_aromyl_gr = tk.Label(self, text='0 gr', font=('Verdana', 15))
-        
+
         self.lbl_acqua = tk.Label(self, text='ACQUA', font=('Verdana', 15))
 
         style = ttk.Style()
-        style.configure('W.TButton', font =('Verdana', 12, 'bold'))
-        
-        self.calcola = ttk.Button(self, text='CALCOLA', style = 'W.TButton', command=_calcola)
-        self.reset = ttk.Button(self, text='Reset', style = 'W.TButton', command=_reset)
-        self.chiudi = ttk.Button(self, text='Chiudi', style = 'W.TButton', command=_chiudi)
-        
+        style.configure('W.TButton', font=('Verdana', 12, 'bold'))
+
+        self.calcola = ttk.Button(self, text='CALCOLA', style='W.TButton', command=_calcola)
+        self.reset = ttk.Button(self, text='Reset', style='W.TButton', command=_reset)
+        self.chiudi = ttk.Button(self, text='Chiudi', style='W.TButton', command=_chiudi)
+
         # LAYOUT
         self.lbl_peso.grid(row=0, column=0, sticky='ns')
         self.ent_peso.grid(row=0, column=1)
@@ -82,10 +84,10 @@ class Dosi(tk.Toplevel):
 
         self.lbl_aglio.grid(row=4, column=0)
         self.lbl_aglio_gr.grid(row=4, column=1)
-        
+
         self.lbl_aromyl.grid(row=5, column=0)
         self.lbl_aromyl_gr.grid(row=5, column=1)
-        
+
         self.lbl_acqua.grid(row=6, column=0)
 
         self.calcola.grid(row=7, column=0, columnspan=2, sticky='we')
